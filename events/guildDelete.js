71b071ea72771fdb6bc1de6ad12c59fb383475c1
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2021-07-20 16:20:44 +0200

    Update and rename guildLeave.js to guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+const client = require("../index")
+client.on('guildDelete', guild =>{
+    const channelId = '867023143737884680';
+    const channel = client.channels.cache.get(channelId); 
+    
+    if(!channel) return;  
+    const leave = new MessageEmbed()
+        .setTitle('I Left A Guild!')
+        .setDescription(`Guild`, `Name ${guild.name}
+        Id ${guild.id}`)
+        .addField(`Members`, `${guild.memberCount}`)
+        .addField(`Channels`, `${guild.channelCount}`)
+        .addField(`Roles`, `${guild.roleCount}`)
+        .addField(`Emojis`, `${guild.emojiCount}`)
+        .setTimestamp()
+        .setColor('RANDOM')
+        .setFooter(`${client.guilds.cache.size} Guilds
+        ${client.users.cache.size} Members!`);
+
+    channel.send(leave);
+});
